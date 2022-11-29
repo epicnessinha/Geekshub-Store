80d6337d6512dcd30975ca2aeadce3bafc2c150b
Vanessa Ferreira <vanessa.ferreira.dev@gmail.com>
2022-11-29 09:58:29 +0000

    Product detail fixed

--- src/containers/ProductDetails/ProductDetails.jsx
+++ src/containers/ProductDetails/ProductDetails.jsx
@@ -7,16 +7,25 @@ const ProductDetails = () => {
 
   return (
     <div className="productDetailDesign">
-      <br></br>
-      <h1>Product Detail</h1>
-      {product.id !== undefined && (
-        <div className="productTitle">{product.title}</div>
-      )}
+    <div className="title">
+     <h1>{product.title}</h1> 
+     </div>
+        <div><img className="image" src={product.images} alt={product.title} width='350' height='300'/></div>
+        <div className='priceAndDescription'>
+            <div>Price: {product.price} €</div>
+            <div className="productDescription">
+                <div>{product.description}</div>
+                </div>
+        </div>
+
+      
     </div>
   );
 };
 
 export default ProductDetails;
+
+
 
 
 /* <div><img className='productImage' src={product.image}} alt={product.image} with='375' height='560'/></div>
